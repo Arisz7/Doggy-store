eeb36e777f90d15eb46c233c92a933e10af53cf7
Arisz7 <nannyvaldez@hotmail.com>
2022-11-30 16:54:47 -0800

    day 1

--- src/root.tsx
+++ src/root.tsx
@@ -1,8 +1,12 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import { component$, useStyles$ } from "@builder.io/qwik";
+import {
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
 
-import globalStyles from './global.css?inline';
+import globalStyles from "./global.css?inline";
 
 export default component$(() => {
   /**
@@ -19,8 +23,25 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com
+          /ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <script
+          src="https://kit.fontawesome.com/13b9d63802.js"
+          crossorigin="anonymous"></script>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200&display=swap"
+          rel="stylesheet"
+        ></link>
       </head>
-      <body lang="en">
+      <body lang="en" class="flex flex-col min-h-screen">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
